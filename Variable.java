0845a9b263910fa5f4bd6da997ae0f76f9a6a3a8
nehadalvi <nehadalavi317@gmail.com>
2026-01-10 19:35:50 +0530

    Day -3 Java Variables

---
+++ Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    public static void main (String []args)
+    {
+        int age =10;
+        String name= "neha";
+        char div ='A';
+        double salary = 50000.00;
+        System.out.println("Age = "+age);
+        System.out.println("Name is = "+name);
+
+        System.out.println("Division = "+div);
+
+        System.out.println("salary = "+salary);
+
+    }
+}
