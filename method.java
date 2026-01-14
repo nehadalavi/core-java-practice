d32f7de1f87646872f18312454ad44cba183432a
nehadalvi <nehadalavi317@gmail.com>
2026-01-14 13:30:29 +0530

    Day-7 This is a simple java Method

---
+++ method.java
@@ -0,0 +1,19 @@
+public class method {
+    
+
+    // An example method
+    public void printMessage() {
+        System.out.println("This is simple java method!");
+    }
+    
+    public static void main(String[] args) {
+        
+        // Create an instance of the class
+        // containing the method
+        method obj = new method();
+        
+        // Calling the method
+        obj.printMessage(); 
+    }
+}
+
