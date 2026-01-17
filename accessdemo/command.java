fcbf9da30a7e540558bce40594c3010f372955bc
nehadalvi <nehadalavi317@gmail.com>
2026-01-17 20:23:49 +0530

    Day-10 command line in java

---
+++ accessdemo/command.java
@@ -0,0 +1,27 @@
+package accessdemo;
+
+class Geeks {
+
+    // Main driver method
+    public static void main(String[] args)
+    {
+        // Checking if length of args array is
+        // greater than 0
+        if (args.length > 0) {
+
+            // Print statements
+            System.out.println("The command line"
+                               + " arguments are:");
+
+            // Iterating the args array
+            // using for each loop
+            for (String val : args)
+
+                System.out.println(val);
+        }
+        else
+
+            System.out.println("No command line "
+                               + "arguments found.");
+    }
+}
