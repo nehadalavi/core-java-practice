e7dd23c987c80ceff3ba3d86e28c16fbafa8b3c6
nehadalvi <nehadalavi317@gmail.com>
2026-01-20 13:12:56 +0530

    Day-13 it is simple array declaration and initialization

---
+++ arraydeclaration.java
@@ -0,0 +1,9 @@
+class ArrayExample {
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 15};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
