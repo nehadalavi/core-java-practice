0f91a7e21b74dcbe6242966100de96f909c067f1
nehadalvi <nehadalavi317@gmail.com>
2026-01-26 14:01:31 +0530

    Day 16 Array class

---
+++ Arrayclass.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class ArraysDemo {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 7, 1};
+
+        Arrays.sort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+}
