ac7a91f80b75c89be4f70c56d9f7919d743be4b2
nehadalvi <nehadalavi317@gmail.com>
2026-02-07 13:26:28 +0530

    Day 26 Exception Handling In java

---
+++ exception.java
@@ -0,0 +1,11 @@
+public class exception {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Exception handled");
+        } finally {
+            System.out.println("Finally block executed");
+        }
+    }
+}
