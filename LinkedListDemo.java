622ad962d3e7e6e041f13e886a9655b9b7b70dc9
nehadalvi <nehadalavi317@gmail.com>
2026-02-21 20:52:22 +0530

    Day 34

---
+++ LinkedListDemo.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+    public static void main(String[] args){
+        
+        // Creating a LinkedList
+        LinkedList<String> l = new LinkedList<String>();
+
+        // Adding elements
+        l.add("One");
+        l.add("Two");
+        l.add("Three");
+        l.add("Four");
+        l.add("Five");
+
+        System.out.println(l);
+    }
+}
